96033ee80a2101f0263cdbf3193e0bbceb38edda
Aryan Singh <75606005+10aryan@users.noreply.github.com>
2024-05-24 12:00:46 +0000

    data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+// alert(3 + 3) we are using node js not browser
+
+console.log(3 +3); console.log("aryan");// code readability be high
+
+
+let name = "arryan"
+let age = 21
+let isLoggIn = false
+
+// number ==> 2 to power 53
+// bigint is use for large no
+// String ==> ""
+// boolean ==> true/false
+// null ==> stand alone value
+// undifined ==> 
+// symbol ==> unique
+
+// object
+
+console.log(typeof "aryan");//String
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
