d2b2c11261a3469a63638b453c9a0b5d0e6613e2
Aryan Singh <75606005+10aryan@users.noreply.github.com>
2024-05-25 09:00:00 +0000

    Conversion in js

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,27 @@
+let score = ""
+
+console.log(typeof score);
+
+let valueNumber = Number(score)
+
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//"33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1; flase ==> 0
+
+let isLoggedIn ="aryan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 ==> true; 0 ==> flase
+// "" ==> false
+// "aryan" ==> true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
