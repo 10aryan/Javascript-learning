582efd94389945a7165bdd2be4a081738e82332d
Aryan Singh <75606005+10aryan@users.noreply.github.com>
2024-05-25 09:12:35 +0000

    operation in js

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -1,27 +1,66 @@
-let score = ""
+// let score = ""
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueNumber = Number(score)
+// let valueNumber = Number(score)
 
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
-//"33" ==> 33
-// "33abc" ==> NaN
-// true ==> 1; flase ==> 0
+// //"33" ==> 33
+// // "33abc" ==> NaN
+// // true ==> 1; flase ==> 0
 
-let isLoggedIn ="aryan"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn ="aryan"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-// 1 ==> true; 0 ==> flase
-// "" ==> false
-// "aryan" ==> true
+// // 1 ==> true; 0 ==> flase
+// // "" ==> false
+// // "aryan" ==> true
 
-let someNumber = 33
+// let someNumber = 33
 
-let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+// ***************************************Operation***********************************************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 ="heloo"
+let str2 = " aryan"
+
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 +"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+++gameCounter
+console.log(gameCounter);
